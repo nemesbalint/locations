5b0bd8c6aef456d7cfa4c86862f39a45d5f72a55
Alerant_NemesB <balint.nemes@erstebank.hu>
2023-05-25 22:58:17 +0200

    Spring Data JPA

--- src/main/java/locations/Location.java
+++ src/main/java/locations/Location.java
@@ -1,6 +1,12 @@
 package locations;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,14 +15,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Schema(description = "Represents a location.")
+@Entity
+@Table(name = "locations")
 public class Location {
     @Schema(description = "The unique identifier of the location.")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Schema(description = "The name of the location.", example = "Name")
+    @Column
     private String name;
     @Schema(description = "The latitude of the location.", example = "51.5074")
+    @Column
     private double lat;
     @Schema(description = "The longitude of the location.", example = "-0.1278")
+    @Column
     private double lon;
 
     /**
--- src/main/java/locations/LocationsService.java
+++ src/main/java/locations/LocationsService.java
@@ -1,5 +1,6 @@
 package locations;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,12 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class LocationsService {
 
-    private LocationsDao locationsDao;
+    private LocationsRepository repository;
     private LocationMapper locationMapper;
     private LocationsProperties locationsProperties;
 
     public List<LocationDto> listLocations(Optional<String> prefix, Optional<Double> minLat) {
-        List<Location> filtered = locationsDao.findAll().stream()
+        List<Location> filtered = repository.findAll().stream()
                 .filter(e -> (prefix.isEmpty() || e.getName().toLowerCase().startsWith(prefix.get().toLowerCase()))
                         && (minLat.isEmpty() || e.getLat() >= minLat.get())
                 )
@@ -28,7 +29,7 @@ public class LocationsService {
 
     public LocationDto findLocationById(long id) {
         log.debug("findLocationById called with id: {} ", id);
-        return locationMapper.toDto(locationsDao.findById(id));
+        return locationMapper.toDto(repository.findById(id).orElseThrow(()-> new IllegalArgumentException("location not found") ));
     }
 
     public LocationDto createLocation(CreateLocationCommand command) {
@@ -38,28 +39,28 @@ public class LocationsService {
                 command.getLat(),
                 command.getLon());
         log.debug("createLocation called with location: {}", location);
-        locationsDao.createLocation(location);
+        repository.save(location);
         return locationMapper.toDto(location);
     }
 
+    @Transactional
     public LocationDto updateLocation(long id, UpdateLocationCommand command) {
         log.debug("updateLocation called with command: {}", command);
-        Location location = locationsDao.findById(id);
+        Location location = repository.findById(id).orElseThrow(()->new IllegalArgumentException("location not found"));
         location.setName( locationsProperties.isNameAutoUpperCase() ? command.getName().toUpperCase() : command.getName());
         location.setLat(command.getLat());
         location.setLon(command.getLon());
-        locationsDao.updateLocation(location);
         return locationMapper.toDto(location);
     }
 
     public void deleteLocation(long id) {
         log.debug("deleteLocation called with id: {}", id);
-        locationsDao.deleteLocation(id);
+        repository.deleteById(id);
     }
 
     public void deleteAllLocations() {
         log.debug("deleteAllLocations called");
-        locationsDao.deleteAllLocation();
+        repository.deleteAll();
     }
 
 }
