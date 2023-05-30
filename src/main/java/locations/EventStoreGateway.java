63cd287257960f2c2872eb31a8405c153a617c1b
Alerant_NemesB <balint.nemes@erstebank.hu>
2023-05-30 23:12:20 +0200

    WebClient

--- src/main/java/locations/EventStoreGateway.java
+++ src/main/java/locations/EventStoreGateway.java
@@ -1,27 +1,31 @@
 package locations;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import eventstore.CreateEventCommand;
 import eventstore.EventDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
 public class EventStoreGateway {
-    private final RestTemplate restTemplate;
 
     @Autowired
     private LocationsProperties locationsProperties;
 
-    public EventStoreGateway(RestTemplateBuilder builder) {
-        this.restTemplate = builder.build();
+    public EventDto createEvent (CreateEventCommand command) {
+        return WebClient.create(getBaseUrl(locationsProperties.getEventStoreUrl()))
+                .post()
+                .uri("/api/events")
+                .body(Mono.just(command), CreateEventCommand.class)
+                .retrieve()
+                .bodyToMono(EventDto.class)
+                .block();
     }
 
-    public EventDto createEvent (CreateEventCommand command) {
-        return restTemplate.postForObject(locationsProperties.getEventStoreUrl(), command, EventDto.class);
+    private String getBaseUrl(String eventStoreUrl) {
+        return eventStoreUrl.substring(0, eventStoreUrl.indexOf("/api"));
     }
 }
