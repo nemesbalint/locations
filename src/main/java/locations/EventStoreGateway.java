f43cb6c596ffb41fa88b45d34390b4574e456375
Alerant_NemesB <balint.nemes@erstebank.hu>
2023-05-31 22:35:47 +0200

    RestTemplate integration test

--- src/main/java/locations/EventStoreGateway.java
+++ src/main/java/locations/EventStoreGateway.java
@@ -3,7 +3,6 @@ package locations;
 import eventstore.CreateEventCommand;
 import eventstore.EventDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -11,12 +10,20 @@ import reactor.core.publisher.Mono;
 @Service
 @Slf4j
 public class EventStoreGateway {
-
-    @Autowired
-    private LocationsProperties locationsProperties;
+//
+//    private final RestTemplate restTemplate;
+//
+//
+//    public EventStoreGateway(RestTemplateBuilder builder) {
+//        this.restTemplate = builder.build();
+//    }
+//
+//    public EventDto createEvent (CreateEventCommand command) {
+//        return restTemplate.postForObject("http://localhost:8081/api/events", command, EventDto.class);
+//    }
 
     public EventDto createEvent (CreateEventCommand command) {
-        return WebClient.create(getBaseUrl(locationsProperties.getEventStoreUrl()))
+        return WebClient.create(getBaseUrl("http://localhost:8081/api/events"))
                 .post()
                 .uri("/api/events")
                 .body(Mono.just(command), CreateEventCommand.class)
